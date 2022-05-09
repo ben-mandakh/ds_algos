ede312f3aaab2f1c5194371e1375a4221865627e
Ben Mandakh <benmandakh@gmail.com>
2022-05-09 16:50:29 -0500

    added a function

--- research.js
+++ research.js
@@ -1,3 +1,8 @@
 const name = () => {
     console.log("this is also test as of now ...")
 }
+//// Adding more functions here now
+
+const add = (x,y) => {
+    return x + y   //// pretty good enough now.. just testing with someone
+}
