fe8845f255c67b6809c294469774a197520cc668
Ben Mandakh <benmandakh@gmail.com>
2022-06-04 13:07:11 -0500

    adding an variable

--- research.js
+++ research.js
@@ -1,11 +1,14 @@
-const name = () => {
-    console.log("this is also test as of now ...")
-}
-//// Adding more functions here now
+// const name = () => {
+//     console.log("this is also test as of now ...")
+// }
+// //// Adding more functions here now
 
-const add = (x,y) => {
-    return x + y   //// pretty good enough now.. just testing with someone
-}
+// const add = (x,y) => {
+//     return x + y   //// pretty good enough now.. just testing with someone
+// }
 
-const sub = (x,y) => {
-    return x - y
+// const sub = (x,y) => {
+//     return x - y
+
+const major = ['tester', 'QA', 'developer', 'chef']
+
